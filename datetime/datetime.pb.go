7e5e765de0f3b1bb6d4ab2b4caefe2f103ad13e5
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-04-08 11:28:03 +0700

    Add method GetDatetimeStream

--- datetime/datetime.pb.go
+++ datetime/datetime.pb.go
@@ -104,17 +104,18 @@ func init() {
 func init() { proto.RegisterFile("datetime.proto", fileDescriptor_f3a0163ec53b86cf) }
 
 var fileDescriptor_f3a0163ec53b86cf = []byte{
-	// 153 bytes of a gzipped FileDescriptorProto
+	// 171 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4b, 0x49, 0x2c, 0x49,
 	0x2d, 0xc9, 0xcc, 0x4d, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x80, 0xf1, 0xa5, 0xe4,
 	0xd3, 0xf3, 0xf3, 0xd3, 0x73, 0x52, 0xf5, 0xc1, 0xe2, 0x49, 0xa5, 0x69, 0xfa, 0x20, 0xd1, 0xe2,
 	0x92, 0xc4, 0xdc, 0x02, 0x88, 0x52, 0x25, 0x01, 0x2e, 0x3e, 0x17, 0xa8, 0xe2, 0x80, 0xc4, 0xa2,
 	0xc4, 0xdc, 0x62, 0x25, 0x77, 0x2e, 0x5e, 0x98, 0x48, 0x50, 0x6a, 0x41, 0x4e, 0xa5, 0x90, 0x19,
 	0x17, 0xdc, 0x3c, 0x09, 0x46, 0x05, 0x46, 0x0d, 0x6e, 0x23, 0x29, 0x3d, 0x88, 0xb1, 0x7a, 0x30,
-	0x63, 0xf5, 0x42, 0x60, 0xc6, 0x06, 0xc1, 0xd5, 0x1a, 0xf9, 0x71, 0x71, 0xc0, 0x0c, 0x12, 0x72,
-	0xe2, 0xe2, 0x76, 0x4f, 0x2d, 0x81, 0x73, 0x25, 0xf4, 0xe0, 0x2e, 0x46, 0xb5, 0x5d, 0x4a, 0x1c,
-	0x53, 0x06, 0xec, 0x0a, 0x25, 0x86, 0x24, 0x36, 0xb0, 0x6d, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff,
-	0xff, 0x25, 0x7f, 0x7d, 0xbf, 0xee, 0x00, 0x00, 0x00,
+	0x63, 0xf5, 0x42, 0x60, 0xc6, 0x06, 0xc1, 0xd5, 0x1a, 0xcd, 0x62, 0xe4, 0xe2, 0x80, 0x99, 0x24,
+	0xe4, 0xc4, 0xc5, 0xed, 0x9e, 0x5a, 0x02, 0xe7, 0x4a, 0xe8, 0xc1, 0x9d, 0x8c, 0x6a, 0xbd, 0x94,
+	0x38, 0xa6, 0x0c, 0xd8, 0x19, 0x4a, 0x0c, 0x42, 0x5e, 0x5c, 0x82, 0x48, 0x66, 0x04, 0x97, 0x14,
+	0xa5, 0x26, 0xe6, 0x92, 0x65, 0x92, 0x01, 0x63, 0x12, 0x1b, 0xd8, 0xe9, 0xc6, 0x80, 0x00, 0x00,
+	0x00, 0xff, 0xff, 0x7b, 0x23, 0x6d, 0x89, 0x3b, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -131,6 +132,7 @@ const _ = grpc.SupportPackageIsVersion6
 type DatetimeClient interface {
 	// Sends a greeting
 	GetDatetime(ctx context.Context, in *DatetimeParams, opts ...grpc.CallOption) (*DatetimeReply, error)
+	GetDatetimeStream(ctx context.Context, in *DatetimeParams, opts ...grpc.CallOption) (Datetime_GetDatetimeStreamClient, error)
 }
 
 type datetimeClient struct {
@@ -150,10 +152,43 @@ func (c *datetimeClient) GetDatetime(ctx context.Context, in *DatetimeParams, op
 	return out, nil
 }
 
+func (c *datetimeClient) GetDatetimeStream(ctx context.Context, in *DatetimeParams, opts ...grpc.CallOption) (Datetime_GetDatetimeStreamClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Datetime_serviceDesc.Streams[0], "/datetime.Datetime/GetDatetimeStream", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &datetimeGetDatetimeStreamClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Datetime_GetDatetimeStreamClient interface {
+	Recv() (*DatetimeReply, error)
+	grpc.ClientStream
+}
+
+type datetimeGetDatetimeStreamClient struct {
+	grpc.ClientStream
+}
+
+func (x *datetimeGetDatetimeStreamClient) Recv() (*DatetimeReply, error) {
+	m := new(DatetimeReply)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // DatetimeServer is the server API for Datetime service.
 type DatetimeServer interface {
 	// Sends a greeting
 	GetDatetime(context.Context, *DatetimeParams) (*DatetimeReply, error)
+	GetDatetimeStream(*DatetimeParams, Datetime_GetDatetimeStreamServer) error
 }
 
 // UnimplementedDatetimeServer can be embedded to have forward compatible implementations.
@@ -162,6 +197,9 @@ type UnimplementedDatetimeServer struct {
 
 func (*UnimplementedDatetimeServer) GetDatetime(ctx context.Context, req *DatetimeParams) (*DatetimeReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetDatetime not implemented")
+}
+func (*UnimplementedDatetimeServer) GetDatetimeStream(req *DatetimeParams, srv Datetime_GetDatetimeStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetDatetimeStream not implemented")
 }
 
 func RegisterDatetimeServer(s *grpc.Server, srv DatetimeServer) {
@@ -186,6 +224,27 @@ func _Datetime_GetDatetime_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Datetime_GetDatetimeStream_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(DatetimeParams)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(DatetimeServer).GetDatetimeStream(m, &datetimeGetDatetimeStreamServer{stream})
+}
+
+type Datetime_GetDatetimeStreamServer interface {
+	Send(*DatetimeReply) error
+	grpc.ServerStream
+}
+
+type datetimeGetDatetimeStreamServer struct {
+	grpc.ServerStream
+}
+
+func (x *datetimeGetDatetimeStreamServer) Send(m *DatetimeReply) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 var _Datetime_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "datetime.Datetime",
 	HandlerType: (*DatetimeServer)(nil),
@@ -195,6 +254,12 @@ var _Datetime_serviceDesc = grpc.ServiceDesc{
 			Handler:    _Datetime_GetDatetime_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "GetDatetimeStream",
+			Handler:       _Datetime_GetDatetimeStream_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "datetime.proto",
 }
